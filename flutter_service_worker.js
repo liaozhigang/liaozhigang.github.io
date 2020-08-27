486e7da2c5f45fdbcc3c1b485db5a6c687279803
zliao <liao_zg@zzvc.com.cn>
2020-08-27 17:08:17 +0800

    progress

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,7 +19,7 @@ const RESOURCES = {
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "manifest.json": "dc4d0d9da3c1b3f98c8a79ed228a6469",
-"main.dart.js": "aa6f0b333a12a25416c4bebb98300e11"
+"main.dart.js": "063f6331bf74dcbe5b8c138b1e78b9e1"
 };
 
 // The application shell files that are downloaded before a service worker can
